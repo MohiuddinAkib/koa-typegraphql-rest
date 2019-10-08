35d3e4c0c828ed30c95a647d38aefb824ca37ce2
md akib <mohiuddinmostafakamal@gmail.com>
2019-10-09 02:14:35 +0600

    dotenv import issue

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -1,7 +1,8 @@
 import path from 'path';
-import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+
   dotenv.config({
     path: path.resolve(process.cwd(), '.env.development')
   });
